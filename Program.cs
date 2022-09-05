5189c8a69dc66910257e0c2294827fdb0bfe5e57
Henrik Vincents Poulsen <hpo@tec.dk>
2022-09-05 13:18:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using OOP009Encapsulation;
+
+Skoletaske AlexandersTaske = new();
+Madkasse AMadkasse = new Madkasse();
+AlexandersTaske.FoodBox = AMadkasse;
+Madder Spegepølsemad = new Madder();
+Spegepølsemad.Name = "Sigurd Spegepølse";
+AMadkasse.MadderList.Add(Spegepølsemad);
+AlexandersTaske.Penalhus = new Penalhus();
+AlexandersTaske.Penalhus.Viskelæderlist = new();
+AlexandersTaske.Penalhus.Viskelæderlist.Add(new Viskelæder() { Name = "Jordbær"});
+
+
+foreach (Viskelæder viskelæder in AlexandersTaske.Penalhus.Viskelæderlist)
+{
+    Console.WriteLine("Viskelæder: " + viskelæder.Name);
+}
+
+foreach (Madder madder in AlexandersTaske.FoodBox.MadderList)
+{
+    Console.WriteLine("Madder: " + madder.Name);
+}
---
+++ SchoolStuff.cs
@@ -0,0 +1,41 @@
+﻿namespace OOP009Encapsulation
+{
+    internal class SchoolStuff
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Condition { get; set; }
+    }
+
+    internal class Skoletaske : SchoolStuff
+    {
+        public Penalhus Penalhus { get; set; }
+        public List<Bøger> Booklist { get; set; }
+        public Madkasse FoodBox { get; set; }
+    }
+    internal class Bøger : SchoolStuff
+    {
+        public string Author { get; set; }
+    }
+    internal class Penalhus : SchoolStuff
+    {
+        public List<Blyanter> Blyantlist { get; set; }
+        public List<Viskelæder> Viskelæderlist { get; set; }
+    }
+    internal class Viskelæder : SchoolStuff
+    {
+        public string Smell { get; set; }
+    }
+    internal class Blyanter : SchoolStuff
+    {
+        public string Color { get; set; }
+    }
+    internal class Madkasse : SchoolStuff
+    {
+        public List<Madder> MadderList { get; set; } = new();
+    }
+    internal class Madder : SchoolStuff
+    {
+        public string Smag { get; set; }
+    }
+}
